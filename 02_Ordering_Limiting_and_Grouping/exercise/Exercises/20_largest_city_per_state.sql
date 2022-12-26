0ebf9f8ee4096e68ed346693237e749166d0d2d4
Julia <julia.leigh98@gmail.com>
2022-12-26 15:09:41 -0500

    Ordering_Limiting_and_Grouping

--- 02_Ordering_Limiting_and_Grouping/exercise/Exercises/20_largest_city_per_state.sql
+++ 02_Ordering_Limiting_and_Grouping/exercise/Exercises/20_largest_city_per_state.sql
@@ -1,4 +1,6 @@
 -- 20. The state abbreviation, and population of the city with the largest population (name column 'city_population') for all states, territories, and districts.
 -- Order the results from highest to lowest populations.
 -- (56 rows)
-
+SELECT state_abbreviation, population AS city_population
+FROM city
+WHERE population = (SELECT MAX(population) FROM city); 

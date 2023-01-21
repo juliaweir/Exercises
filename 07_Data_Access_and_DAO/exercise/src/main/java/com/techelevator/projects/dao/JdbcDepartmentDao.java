8e3dc2fad6f962711f2c8e2d8ae97199aee2723d
Julia Weir <98445396+juliaweir@users.noreply.github.com>
2023-01-20 23:11:50 -0500

    method mapRowToDepartment, getAllDepartments 1/17passed

--- 07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcDepartmentDao.java
+++ 07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcDepartmentDao.java
@@ -11,8 +11,9 @@ import org.springframework.jdbc.support.rowset.SqlRowSet;
 import com.techelevator.projects.model.Department;
 
 public class JdbcDepartmentDao implements DepartmentDao {
-	
+
 	private final JdbcTemplate jdbcTemplate;
+
 
 	public JdbcDepartmentDao(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -20,17 +21,44 @@ public class JdbcDepartmentDao implements DepartmentDao {
 
 	@Override
 	public Department getDepartment(int id) {
-		return new Department(0, "Not Implemented Yet");
+		//call or instantiate a new department object
+		Department department = null; //null accounts for null pointer exception
+		//your sql code stored in a string
+		String sql = "SELECT department_id, name "
+				+ "FROM department " +
+				"WHERE name = ?;";
+
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+		System.out.println(results);
+		if (results.next()) {
+			department = mapRowToDepartment(results);
+		}
+		return department;
+	}
+
+	private Department mapRowToDepartment(SqlRowSet rowSet) {
+		Department department = new Department();
+		department.setName(rowSet.getString("name"));
+		department.setId(rowSet.getInt("department_id"));
+		return department;
 	}
 
 	@Override
 	public List<Department> getAllDepartments() {
-		return new ArrayList<>();
+		List<Department> departmentList = new ArrayList<>();
+		String sql = "SELECT department_id, name " +
+				"FROM department ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+		while (results.next()){
+			departmentList.add(mapRowToDepartment(results));
+		}
+
+		return departmentList;
 	}
 
 	@Override
 	public void updateDepartment(Department updatedDepartment) {
 
-	}
 
+	}
 }

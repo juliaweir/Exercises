4605c934ae224407bee3d208658abc60703b6ec5
Julia Weir <98445396+juliaweir@users.noreply.github.com>
2023-01-22 00:24:16 -0500

    Finished JbdcDepartmentDao

--- 07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcEmployeeDao.java
+++ 07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcEmployeeDao.java
@@ -3,6 +3,7 @@ package com.techelevator.projects.dao;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import javax.sql.DataSource;
 
@@ -21,43 +22,65 @@ public class JdbcEmployeeDao implements EmployeeDao {
 
 	@Override
 	public List<Employee> getAllEmployees() {
+		//create a new list for all employees
 		List<Employee> allEmployees = new ArrayList<>();
-		String sql = "SELECT employee_id, first_name, last_name " +
-				"FROM employee ";
+		//sql query to find all employees
+		String sql = "SELECT employee_id, department_id, first_name, last_name " +
+				"birth_date, hire_date " + "FROM employee; ";
+		//capture results
 		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+		//add them to a list
 		while (results.next()) {
 			allEmployees.add(mapToEmployee(results));
-			System.out.println(results);
 		}
-
 		return allEmployees;
 	}
 
 	@Override
 	public List<Employee> searchEmployeesByName(String firstNameSearch, String lastNameSearch) {
-		List<Employee> returnedSearchByName = new ArrayList<>();
-		String sql = "SELECT first_name, last_name " +
-				"FROM employee + ";
-		//something to make search case insensitive, ilike statement
-		return List.of(new Employee());
+		List<Employee> employees = new ArrayList<>();
+		String sql = "SELECT employee_id, department_id," +
+				"first_name, last_name, birth_date, hire_date " +
+				"FROM employee " +
+				"WHERE first_name ILIKE ? AND last_name ILIKE ?"; //lets you search case insensitive
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql, firstNameSearch, lastNameSearch);
+		while (results.next()) {
+			employees.add(mapToEmployee(results));
+		}
+		return employees;
 	}
 
 	@Override
 	public List<Employee> getEmployeesByProjectId(int projectId) {
-		return new ArrayList<>();
+		List<Employee> employees = new ArrayList<>();
+		String sql = "SELECT e.employee_id, e.department_id, e.first_name, e.last_name, e.birth_date, e.hire_date "+
+				"FROM employee e " +
+				"JOIN project_employee pe ON e.employee_id = pe.employee_id " +
+				"WHERE pe.project_id = ?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql, projectId);
+		while(results.next()){
+			employees.add(mapToEmployee(results));
+		}
+		return employees;
 	}
 
 	@Override
 	public void addEmployeeToProject(int projectId, int employeeId) {
+		String sql = "INSERT INTO project_emplyee (project_id, employee_id) " +
+				"VALUES (?, ?)";
+		jdbcTemplate.update(sql, projectId, employeeId);
 	}
 
 	@Override
 	public void removeEmployeeFromProject(int projectId, int employeeId) {
+		String sql = "DELETE FROM project_employee " +
+				"WHERE project_id = ? AND employee_id = ?";
+		jdbcTemplate.update(sql, projectId, employeeId);
 	}
 
 	@Override
 	public List<Employee> getEmployeesWithoutProjects() {
-		return new ArrayList<>();
+		return null;
 	}
 
 	private Employee mapToEmployee(SqlRowSet rowSet) {
